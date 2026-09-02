4cb70d20e318d38442768caae5e050fa0e7ff502
Juan Daniel Garzon Solaque <danielgarzonjuan12@gmail.com>
2026-09-02 18:54:11 -0500

    [vista][02/09] agrega formulario de creacion de personaje (Gestor RPG - personajes.html)

--- rpg-manager-backend/src/main/java/com/edu/udistrital/gestionrpg/rpg/manager/backend/RpgManagerBackend.java
+++ rpg-manager-backend/src/main/java/com/edu/udistrital/gestionrpg/rpg/manager/backend/RpgManagerBackend.java
@@ -1,30 +1,43 @@
 package com.edu.udistrital.gestionrpg.rpg.manager.backend;
 
-import io.javalin.Javalin;
-import co.edu.udistrital.gestionrpg.modelo.RepositorioPersonajes;
 import co.edu.udistrital.gestionrpg.modelo.Personaje;
+import co.edu.udistrital.gestionrpg.modelo.RepositorioPersonajes;
+import io.javalin.Javalin;
 
         
 public class RpgManagerBackend {
     public static void main(String[] args) {
-        // Javalin puerto 7070
-        Javalin app = Javalin.create().start(7070);
-        
-        // Ruta GET para obtener la lista de personajes
+        // Javalin puerto 7070 habilitando la carpeta de archivos estaticos "public"
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public"); //Lee los archivos desde la carpeta "public" 
+        }).start(7070);
+
+        // Ruta GET para obtener la lista de personajes en formato JSON
         app.get("/personajes", ctx -> {
             ctx.json(RepositorioPersonajes.getListaPersonajes());
         });
         
+        // Ruta POST para agregar/crear un personaje en la lista de personajes (Formulario HTML)
         app.post("/personajes", ctx -> {
              
-            //1. JSON recibido (Conversión a) -> Un objeto Personaje
-             Personaje nuevo = ctx.bodyAsClass(Personaje.class);
+            //1. Extraccion de cada campo que viene del formulario HTML
+             String nombre = ctx.formParam("nombre");
+             int nivel = Integer.parseInt(ctx.formParam("nivel"));
+             int vida = 100;
+             String clase = ctx.formParam("clase");
              
-            //2. Agregar personaje a RepositorioPersonajes
-            RepositorioPersonajes.getListaPersonajes().add(nuevo);
+            //2. Creacion de un nuevo personaje con los datos extraidos del formulario HTML
+            Personaje nuevo = new Personaje(nombre, nivel, vida, clase);
             
-            //3. Respuesta (Mensaje o personaje creado)
-            ctx.status(201).json(nuevo);
+            //3. Agregamos el nuevo personaje a la lista de personajes
+            RepositorioPersonajes.getListaPersonajes().add(nuevo);
+
+            //4. Redirigimos de vuelta a la vista para ver los cambios
+            ctx.redirect("/personajes.html");
+
+            //5. Redirigimos de vuelta a la vista para ver los cambios y mostrar un mensaje de exito
+            ctx.redirect("/personajes.html?creado=true");
+
         });
     }
 }
